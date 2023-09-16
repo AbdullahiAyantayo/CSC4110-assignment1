ece7baccdd356f652eecdbed972ac188e93c460b
Abdullahi Ayantayo <ayantayoabdullah@gmail.com>
2023-09-15 20:03:45 -0400

    First Commit for Assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,21 @@
+// Simple C++ program to display "Hello World" 
+//Author: Amiangshu Bosu <amiangshu.bosu@wayne.edu>
+  
+// Header file for input output functions 
+
+//Adding comments for fun and conflict
+//Version of master branch
+
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Testing git merge!"; 
+      
+    return 0; 
+} 
